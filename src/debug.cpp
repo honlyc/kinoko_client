322d438c8ccf30a914316ec35bc6cc9d85b799e7
luckyhhe <luckyhhe@tencent.com>
2026-08-29 22:00:35 +0800

    feat: add log

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,5 +1,34 @@
 #include "pch.h"
 #include "debug.h"
+#include <pathcch.h>
+#include <ctime>
+
+
+static void WriteToLogFile(const char* pszMessage) {
+    wchar_t sPath[MAX_PATH];
+    GetModuleFileNameW(nullptr, sPath, MAX_PATH);
+    PathCchRemoveFileSpec(sPath, MAX_PATH);
+    PathCchAppend(sPath, MAX_PATH, L"log");
+    CreateDirectoryW(sPath, nullptr);
+
+    time_t now = time(nullptr);
+    struct tm localtime;
+    localtime_s(&localtime, &now);
+
+    wchar_t sFileName[MAX_PATH];
+    wcsftime(sFileName, MAX_PATH, L"debug_%Y-%m-%d.txt", &localtime);
+    PathCchAppend(sPath, MAX_PATH, sFileName);
+
+    FILE* file = nullptr;
+    if (_wfopen_s(&file, sPath, L"a") != 0 || !file) {
+        return;
+    }
+
+    char sTimeInfo[64];
+    strftime(sTimeInfo, sizeof(sTimeInfo), "%H:%M:%S", &localtime);
+    fprintf_s(file, "[%s] %s\n", sTimeInfo, pszMessage);
+    fclose(file);
+}
 
 
 void DebugMessage(const char* pszFormat, ...) {
@@ -9,6 +38,7 @@ void DebugMessage(const char* pszFormat, ...) {
     va_start(argList, pszFormat);
     StringCbVPrintfA(pszDest, cbDest, pszFormat, argList);
     OutputDebugStringA(pszDest);
+    WriteToLogFile(pszDest);
     va_end(argList);
 }
 
